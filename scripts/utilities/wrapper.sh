ccc667a6adaf399d661aabf32db8e13024ae58cf
Matthew Chivers <matthew.chivers@ibm.com>
2021-08-20 16:50:04 +0100

    Initial work on git stash rework

--- scripts/utilities/wrapper.sh
+++ scripts/utilities/wrapper.sh
@@ -6,8 +6,17 @@ SCRI_DIR="$HOOK_DIR/scripts"
 UTIL_DIR="$SCRI_DIR/utilities"
 CONF_DIR="$SCRI_DIR/configurations"
 
+GIT_STASH_NAME="unstaged-changes$(head /dev/urandom | LC_ALL=C tr -dc A-Za-z0-9 | head -c10 ; echo '')"
+
+git stash push -m $GIT_STASH_NAME
+
 # Check style
 $UTIL_DIR/checkstyle.sh $CONF_DIR || { echo "Style Checks Failed" ; exit 1; }
 
 # Run a gradle build against the project directory
 $UTIL_DIR/gradle-build.sh $PROJ_DIR || { echo "Gradle Build Failed" ; exit 1; }
+
+apply_stash() {
+    STASH_NUMBER="$(git stash list | grep -E "$GIT_STASH_NAME$" | head -n1 | awk -F: '{print $1;}')"
+    git stash apply $STASH_NUMBER
+}
