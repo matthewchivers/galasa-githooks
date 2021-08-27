b1d05338c15866a435fcebf2701c86181b6d0b56
Matthew Chivers <matthew.chivers@ibm.com>
2021-08-27 07:35:43 +0100

    Added support for managers, and implemented some fixes

--- scripts/utilities/checkstyle.sh
+++ scripts/utilities/checkstyle.sh
@@ -2,9 +2,22 @@
 
 CONF_DIR=$1
 
-for file in $(git diff --diff-filter=d --cached --name-only)
+output_title() {
+    echo ""
+    STRING_TO_PRINT=$1
+    STRING_LENGTH=$(expr ${#STRING_TO_PRINT} + 4)
+    ST_TO_PR=$(printf "%-${STRING_LENGTH}s" "*")
+    echo "${ST_TO_PR// /*}"
+    echo "* $STRING_TO_PRINT *"
+    echo "${ST_TO_PR// /*}"
+    echo ""
+}
+
+output_title "Linting Using Checkstyle"
+
+for file in $(git diff --diff-filter=d --cached --name-only | grep -E "\.java")
 do 
-    echo "File: $file"
+    echo "Checking file: $file"
     java -jar $CHECKSTYLE_JAR -c $CONF_DIR/java_checks.xml $file
     if [ $? -ne 0 ] 
     then
--- scripts/utilities/wrapper.sh
+++ scripts/utilities/wrapper.sh
@@ -6,26 +6,41 @@ SCRI_DIR="$HOOK_DIR/scripts"
 UTIL_DIR="$SCRI_DIR/utilities"
 CONF_DIR="$SCRI_DIR/configurations"
 
-if [ "$REPO_DIR" = ""]; then exit 1; fi
-if [ "$PROJ_DIR" = ""]; then exit 1; fi
+if [ $REPO_DIR = ""]; then exit 1; fi
+if [ $PROJ_DIR = ""]; then exit 1; fi
+
+output_title() {
+    echo ""
+    STRING_TO_PRINT=$1
+    STRING_LENGTH=$(expr ${#STRING_TO_PRINT} + 4)
+    ST_TO_PR=$(printf "%-${STRING_LENGTH}s" "*")
+    echo "${ST_TO_PR// /*}"
+    echo "* $STRING_TO_PRINT *"
+    echo "${ST_TO_PR// /*}"
+    echo ""
+}
 
 # Generate a [hopefully unique] name for our stash
 GIT_STASH_NAME="unstaged-changes-$(head /dev/urandom | LC_ALL=C tr -dc A-Za-z0-9 | head -c10 ; echo '')"
 
 apply_stash() {
+    output_title "Retrieving stashed changes."
     # Get stash number that has our unique stash name
     STASH_NUMBER="$(git stash list | grep -E "$GIT_STASH_NAME$" | head -n1 | awk -F: '{print $1;}')"
-    git stash apply $STASH_NUMBER
+    git stash apply -q $STASH_NUMBER
 }
 
 # Ensure we will apply the stash even if failing for some reason
 trap apply_stash EXIT
 
 # Stash unstaged changes to prevent building/linting against files not being included in the commit
-git stash push -m $GIT_STASH_NAME
+output_title "Stashing current changes (ensuring we work on only staged files)."
+git stash push -k -m $GIT_STASH_NAME
 
 # Check style
+output_title "Performing Linting"
 $UTIL_DIR/checkstyle.sh $CONF_DIR || { echo "Style Checks Failed" ; exit 1; }
 
 # Run a gradle build against the project directory
+output_title "Performing Build"
 $UTIL_DIR/gradle-build.sh $PROJ_DIR || { echo "Gradle Build Failed" ; exit 1; }
